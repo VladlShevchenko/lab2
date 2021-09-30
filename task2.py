89aa777b5bda5221ba3d911c1090b3e5f0ddfe16
VladlShevchenko <72383990+VladlShevchenko@users.noreply.github.com>
2021-09-30 14:01:53 +0300

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,37 @@
+from math import gcd
+
+class Rational:
+    def __init__(self, numerator = 0, denominator = 1):
+
+        if not denominator:
+            raise ZeroDivisionError("Division by zero!")
+
+        if not isinstance(numerator, int) or not isinstance(denominator, int):
+            raise TypeError("Numerator and denominator must be integer")
+
+        if(numerator < 0 and denominator < 0):
+            numerator *= -1
+            denominator *= -1
+
+        self.numerator = numerator // gcd(numerator, denominator)
+        self.denominator = denominator // gcd(numerator, denominator)
+
+
+    def division(self):
+        return str(self.numerator) + "/" + str(self.denominator)
+
+    def divisionFloat(self):
+        return self.numerator / self.denominator
+
+
+def main():
+    firstFraction = Rational(2, 1)
+    print(firstFraction.division())
+    print(firstFraction.divisionFloat())
+
+    secondFraction = Rational(4, 1)
+    print(secondFraction.division())
+    print(secondFraction.divisionFloat())
+
+
+main()
